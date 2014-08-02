96f188f31449346a1f6333d45c1f0bac90bf9ed8
William C. Garrison III <bill@cs.pitt.edu>
2014-08-02 18:24:11 -0400

    Dotcase: getSmallNumSprite isn't just numbers anymore
    Change-Id: Ic7212227aa077f3fcc23da0fe7cd430bcc215599

--- dotcase/src/org/cyanogenmod/dotcase/DotcaseConstants.java
+++ dotcase/src/org/cyanogenmod/dotcase/DotcaseConstants.java
@@ -767,7 +767,7 @@ public class DotcaseConstants {
         {-1, -1, -1},
         {-1, -1, -1}};
 
-    static int[][] getSmallNumSprite(char c) {
+    static int[][] getSmallCharSprite(char c) {
         switch (c) {
             case '0':
                 return small0;
